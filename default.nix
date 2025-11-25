b1cb2532f641538c3b648242de1c5c11ad5fc1d7
Jeremy O'Brien <neutral@fastmail.com>
2025-11-25 01:18:12 -0500

    add flake + default.nix

---
+++ default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  installShellFiles,
+  stdenv,
+  ...
+}:
+stdenv.mkDerivation {
+  pname = "ke";
+  version = "1.3.1";
+
+  src = lib.cleanSource ./.;
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp ke $out/bin/
+
+    runHook postInstall
+  '';
+
+  postInstall = ''
+    installManPage ke.1
+  '';
+}
---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Kyle's Text Editor";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      packages.x86_64-linux = {
+        default = pkgs.callPackage ./default.nix { };
+      };
+    };
+}
